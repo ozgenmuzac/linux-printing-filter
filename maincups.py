ae133758b31b1ea1088c09844c58fe77da44f897
ozgenmuzac <ozgen@medrateknoloji.com>
2012-12-21 07:33:42 -0800

    Filter chain is changed. Acl query is introduced.

--- maincups.py
+++ maincups.py
@@ -8,10 +8,11 @@ from socket import socket
 
 class DaemonClient():
 
-	def __init__(self, server, port, job_id):
+	def __init__(self, server, port, job_id, user_name):
 		self.server = server
 		self.port = port
 		self.job_id = job_id
+		self.user_name = user_name
 		self.sock = socket()
 		self.sock.connect((self.server, self.port))
 
@@ -26,30 +27,34 @@ class DaemonClient():
 		if not response.startswith("OK"):
 			return True
 		
-		message = "job_id: " + str(self.job_id)
+		message = "user_name: " + self.user_name
 		response = self.send(message)
 		if not response.startswith("OK"):
+			return True		
+
+		message = "job_id: " + str(self.job_id)
+		response = self.send(message)
+		if response.startswith("OK"):
 			return True
 		else:
-			return True
+			return False
 	
 
-def start_transfering(job_id, args):
-	daemon_client = DaemonClient("127.0.0.1", 9100, job_id)
+def start_transfering(job_id, user_name):
+	daemon_client = DaemonClient("127.0.0.1", 9100, job_id, user_name)
 	fout = tempfile.NamedTemporaryFile(delete=False)
-	daemon_client.send(args)
 	f = sys.stdin
 	text = f.read()
 	fout.write(text)
-	response = daemon_client.send(text)
 	fout.close()
 	response = daemon_client.send_to_daemon(fout.name)
 	if response:
 		sys.stdout.write(text)
 
 if __name__ == '__main__':
-	filelog = open("/home/ozgen/filter.log", "wb+")
+	filelog = open("/home/ozgen/filter.log", "a+b")
 	jobId = int(argv[1])
+	user_name = argv[2]
 	args = argv[0]+" "+argv[1]+" "+argv[2]+" "+argv[3]+" "+argv[4]+" "+argv[5] 
 	filelog.write(argv[0]+" "+argv[1]+" "+argv[2]+" "+argv[3]+" "+argv[4]+" "+argv[5])
-	start_transfering(jobId, args)
+	start_transfering(jobId, user_name)
--- mydlpdaemon.py
+++ mydlpdaemon.py
@@ -2,43 +2,108 @@
 
 import socket
 import cups
+import signal
+import sys
 
 from threading import Thread
 
 class SeapClient():
-	def __init__(self):
-		print "Seap Client initialized"
-	def send_message(self, message):
-		print "Sending Message: " + message
+	def __init__(self, server, port):
+		self.server = server
+		self.port = port
+		self.sock = socket.socket()
+		self.sock.settimeout(10)
+		self.sock.connect((self.server, self.port))
+
+	def send(self, message):
+		for try_count in range(3):
+			try:
+				self.sock.sendall(message+"\r\n")
+				response = self.sock.recv(1024).strip()
+				return response
+			except IOError as why:
+				self.sock = socket()
+				self.sock.settimeout(10)
+				self.sock.connect((self.server, self.port))
+		return ""
+		
+	def acl_query(self, file_path, user_name):
+		try:
+			response = self.send("BEGIN")
+			if not response.startswith("OK"):
+				return True
+			opid = response.split()[1]
+			file_name = file_path.strip("/")[1]
+			response = self.send("SETPROP " + opid + " filename=" + file_name)
+			if not response.startswith("OK"):
+				return True
+			response = self.send("SETPROP " + opid + " destination=printer") #TODO: should be used printer uri
+			if not response.startswith("OK"):
+				return True
+
+			response = self.send("SETPROP " + opid + " burn_after_reading=true")
+			if not response.startswith("OK"):
+				return True
+
+			response = self.send("SETPROP " + opid + " user=" + user_name)
+			if not response.startswith("OK"):
+				return True
+
+			response = self.send("PUSHFILE " + opid + " " + file_path)
+			if not response.startswith("OK"):
+				return True
+
+			response = self.send("END " + opid)
+			if not response.startswith("OK"):
+				return True
+
+			response = self.send("ACLQ " + opid)
+			self.send("DESTROY " + opid)
+			if response.split()[1] == "block":
+				return False
+			else:
+				return True
+		except (IOError, OSError) as why:
+			print "An error occured in aclq"
+
 		
 
 class DaemonAgent(Thread):
 	def __init__(self, conn, addr):
 		self.conn = conn
 		self.claddr = addr
-		self.seap_client = SeapClient()
+		self.seap_client = SeapClient("127.0.0.1", 9099)
 		Thread.__init__(self)
 
 	def run(self):
 		print "Thread Started"
 		inp = self.conn.recv(1024).strip()
-		print inp
 		while inp:
 			inp_arr = inp.split()
-			print inp_arr[0]
 			if inp_arr[0] == "file_path:":
-				self.seap_client.send_message(inp_arr[1])
+				file_path = inp_arr[1]
+				self.conn.send("OK\n")
+				inp = self.conn.recv(1024).strip()
+			elif inp_arr[0] == "user_name:":
+				user_name = inp_arr[1]
 				self.conn.send("OK\n")
 				inp = self.conn.recv(1024).strip()
 			elif inp_arr[0] == "job_id:":
-				print "job_id: " + inp_arr[1]
-				self.conn.send("OK\n")
+				job_id = inp_arr[1]
+				response = self.seap_client.acl_query(file_path, user_name)
+				if response:
+					self.conn.send("OK\n")
+				else:
+					self.conn.send("BLOCK\n")
 				self.conn.close()
 				break
 			else:
-				self.conn.send("OK\n")
 				inp = self.conn.recv(1024).strip()
 		
+
+def signal_handler(signal, frame):
+	s.close()
+	sys.exit(0)
 
 
 HOST = '127.0.0.1'
@@ -46,10 +111,12 @@ PORT = 9100
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
+signal.signal(signal.SIGINT, signal_handler)
+connections = []
 
 while True:
 	conn, addr = s.accept()
 	print 'Connected by', addr
-	
 	a = DaemonAgent(conn, addr)
 	a.start()
+	connections.append(conn)
